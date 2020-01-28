2655c208d30f4a5cfd946b2bea200d29a8e16cd4
delwin dsa <delwin.dsa@axionet.in>
2020-01-28 19:29:56 +0530

    initial commit

---
+++ DatingApp.api/Data/DataContext.cs
@@ -0,0 +1,13 @@
+using DatingApp.api.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingApp.api.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DbContext> options) :base(options) { }
+
+        public DataContext(DbContextOptions options) : base(options){ }
+        public DbSet<Value> Values { get; set;}
+    }
+}
